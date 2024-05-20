602f1f3944132bef82a39262b537b3f37bd5a758
Ksheon <rmldldldl0309@gmail.com>
2024-05-20 17:39:46 +0900

    2024-05-20 17:39

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,3 +5,53 @@ export interface QnaListItem{
     qnaWriterId: string;
     qnaDatetime: string;
 }
+
+export interface ExpenditureListItem{
+    travelScheduleExpenditureDetail: string;
+    travelScheduleExpenditure: number;
+}
+
+export interface ScheduleListItem{
+    scheduleDate: string;
+    scheduleContent: string;
+    scheduleStartTime: string;
+    scheduleEndTime: string;
+}
+
+export interface UserListItem{
+    userId: string;
+    userEmail: string;
+}
+
+export interface TourAttractionsListItem{
+    tourAttractionsImageUrl: string;
+    tourAttractionsName: string;
+    tourAttractionsLocation: string;
+    tourAttractionsTelNumber: string;
+    tourAttractionsHours: string;
+}
+
+export interface RestaurantListItem{
+    restaurantReviewImageUrl: string;
+    restaurantName: string;
+    restaurantLocation: string;
+    restaurantTelNumber: string;
+    restaurantHours: string;
+}
+
+export interface ReviewCommentListItem{
+    reviewCommentNumber: number;
+    reviewCommentWriterId: string;
+    commentContent: string;
+}
+
+export interface ReviewBoardListItem{
+    reviewNumber: number;
+    reviewTitle: string;
+    reviewContent: string;
+    writerId: string;
+    reviewDatetime: string;
+    travelReviewImageUrl: string[];
+    reviewViewCount: number;
+    reviewFavoriteCount: number;
+}
