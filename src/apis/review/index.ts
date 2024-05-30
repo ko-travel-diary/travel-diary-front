9b904696acc92494af7b1ba5cf3d8b65483c8095
Young <anxis001220@gmail.com>
2024-05-31 00:08:59 +0900

    work mypage_mylist_page

--- src/apis/review/index.ts
+++ src/apis/review/index.ts
@@ -1,140 +1,197 @@
 import axios from "axios";
 import { PatchTravelReviewRequestDto, PostTravelReviewCommentRequestDto, PostTravelReviewRequestDto } from "./dto/request";
-import { DELETE_REVIEW_COMMENT_REQUEST_URL, DELETE_REVIEW_REQUEST_URL, GET_COMMENT_LIST_REQUEST_URL, GET_REVIEW_FAVORITE_STATUS_REQUEST_URL, GET_REVIEW_LIST_REQUEST_URL, GET_REVIEW_MY_LIST_REQUEST_URL, GET_REVIEW_REQUEST_URL, GET_TITLE_SEARCH_REVIEW_REQUEST_LIST_URL, GET_WRITER_SEARCH_REVIEW_REQUEST_LIST_URL, GET_WRITE_DATE_SEARCH_REVIEW_REQUEST_LIST_URL, PATCH_FAVORITE_COUNT_REQUEST_URL, PATCH_INCREASE_VIEW_COUNT_REQUEST_URL, PATCH_REVIEW_COMMENT_REQUEST_URL, PATCH_REVIEW_REQUEST_URL, POST_REVIEW_COMMENT_REQUEST_URL, POST_REVIEW_REQUEST_URL } from "src/constant";
+import {
+    DELETE_REVIEW_COMMENT_REQUEST_URL,
+    DELETE_REVIEW_REQUEST_URL,
+    GET_COMMENT_LIST_REQUEST_URL,
+    GET_REVIEW_FAVORITE_STATUS_REQUEST_URL,
+    GET_REVIEW_LIST_REQUEST_URL,
+    GET_REVIEW_MY_LIST_REQUEST_URL,
+    GET_REVIEW_REQUEST_URL,
+    GET_TITLE_SEARCH_REVIEW_REQUEST_LIST_URL,
+    GET_WRITER_SEARCH_REVIEW_REQUEST_LIST_URL,
+    GET_WRITE_DATE_SEARCH_REVIEW_REQUEST_LIST_URL,
+    PATCH_FAVORITE_COUNT_REQUEST_URL,
+    PATCH_INCREASE_VIEW_COUNT_REQUEST_URL,
+    PATCH_REVIEW_COMMENT_REQUEST_URL,
+    PATCH_REVIEW_REQUEST_URL,
+    POST_REVIEW_COMMENT_REQUEST_URL,
+    POST_REVIEW_REQUEST_URL,
+} from "src/constant";
 import { bearerAuthorization, requestErrorHandler, requestHandler } from "..";
 import ResponseDto from "../response.dto";
-import { GetTravelReviewBoardResponseDto, GetTravelReviewCommentListResponseDto, GetTravelReviewDetailResponseDto, GetTravelReviewMyListResponseDto, GetReviewTitleAndContentSearchRequestDto, GetReviewWriterSearchRequestDto, GetReviewWriteDateSearchRequestDto, GetTravelReviewFavoriteStatusResponseDto } from "./dto/response";
+import {
+    GetTravelReviewBoardResponseDto,
+    GetTravelReviewCommentListResponseDto,
+    GetTravelReviewDetailResponseDto,
+    GetTravelReviewMyListResponseDto,
+    GetReviewTitleAndContentSearchRequestDto,
+    GetReviewWriterSearchRequestDto,
+    GetReviewWriteDateSearchRequestDto,
+    GetTravelReviewFavoriteStatusResponseDto,
+} from "./dto/response";
 
 // function : 리뷰 게시물 작성 API 함수
-export const postTravelReviewRequest = async(RequestBody: PostTravelReviewRequestDto, accessToken: string) => {
-    const result = await axios.post(POST_REVIEW_REQUEST_URL, RequestBody, bearerAuthorization(accessToken))
+export const postTravelReviewRequest = async (RequestBody: PostTravelReviewRequestDto, accessToken: string) => {
+    const result = await axios
+        .post(POST_REVIEW_REQUEST_URL, RequestBody, bearerAuthorization(accessToken))
         .then(requestHandler<ResponseDto>)
         .catch(requestErrorHandler);
     return result;
-}
+};
 
 // function : 리뷰 게시물의 댓글 & 답글 작성 API 함수
-export const postTravelReviewCommentRequest = async(reviewNumber: number | string, RequestBody: PostTravelReviewCommentRequestDto, accessToken: string) => {
-    const result = await axios.post(POST_REVIEW_COMMENT_REQUEST_URL(reviewNumber), RequestBody,
-    bearerAuthorization(accessToken))
+export const postTravelReviewCommentRequest = async (
+    reviewNumber: number | string,
+    RequestBody: PostTravelReviewCommentRequestDto,
+    accessToken: string
+) => {
+    const result = await axios
+        .post(POST_REVIEW_COMMENT_REQUEST_URL(reviewNumber), RequestBody, bearerAuthorization(accessToken))
         .then(requestHandler<ResponseDto>)
         .catch(requestErrorHandler);
     return result;
-}
+};
 
 // function : 리뷰 게시물 수정 API 함수
-export const patchTravelReviewRequestDto = async(reviewNumber: number | string, RequestBody: PatchTravelReviewRequestDto, accessToken: string) => {
-    const result = await axios.patch(PATCH_REVIEW_REQUEST_URL(reviewNumber), RequestBody, bearerAuthorization(accessToken))
+export const patchTravelReviewRequestDto = async (reviewNumber: number | string, RequestBody: PatchTravelReviewRequestDto, accessToken: string) => {
+    const result = await axios
+        .patch(PATCH_REVIEW_REQUEST_URL(reviewNumber), RequestBody, bearerAuthorization(accessToken))
         .then(requestHandler<ResponseDto>)
         .catch(requestErrorHandler);
     return result;
-}
+};
 
 // function : 리뷰 게시물의 댓글 & 답글 수정 API 함수
-export const patchTravelCommentRequestDto = async(commentNumber: number | string, RequestBody: PostTravelReviewCommentRequestDto, accessToken: string) => {
-    const result = await axios.post(PATCH_REVIEW_COMMENT_REQUEST_URL(commentNumber), RequestBody,
-    bearerAuthorization(accessToken))
+export const patchTravelCommentRequestDto = async (
+    commentNumber: number | string,
+    RequestBody: PostTravelReviewCommentRequestDto,
+    accessToken: string
+) => {
+    const result = await axios
+        .post(PATCH_REVIEW_COMMENT_REQUEST_URL(commentNumber), RequestBody, bearerAuthorization(accessToken))
         .then(requestHandler<ResponseDto>)
         .catch(requestErrorHandler);
     return result;
-}
+};
 
 // function : 리뷰 게시물의 전체 댓글 & 답글 불러오기 API 함수
-export const getTravelReviewCommentListRequest = async(reviewNumber: number | string) => {
-    const result = await axios.get(GET_COMMENT_LIST_REQUEST_URL(reviewNumber))
+export const getTravelReviewCommentListRequest = async (reviewNumber: number | string) => {
+    const result = await axios
+        .get(GET_COMMENT_LIST_REQUEST_URL(reviewNumber))
         .then(requestHandler<GetTravelReviewCommentListResponseDto>)
         .catch(requestErrorHandler);
     return result;
-}
+};
 
 // function : 리뷰 게시물 목록 불러오기 API 함수
-export const getTravelReviewBoardRequest = async() => {
-    const result = await axios.get(GET_REVIEW_LIST_REQUEST_URL)
+export const getTravelReviewBoardRequest = async () => {
+    const result = await axios
+        .get(GET_REVIEW_LIST_REQUEST_URL)
         .then(requestHandler<GetTravelReviewBoardResponseDto>)
         .catch(requestErrorHandler);
     return result;
-}
+};
 
 // function : 리뷰 게시물 제목 + 내용 검색 목록 불러오기 API 함수
-export const getTravelReviewTitleAndContentSearchRequest = async(searchWord: string) => {
-    const config = {params: {searchWord}}
-    const result = await axios.get(GET_TITLE_SEARCH_REVIEW_REQUEST_LIST_URL, config)
+export const getTravelReviewTitleAndContentSearchRequest = async (searchWord: string) => {
+    const config = { params: { searchWord } };
+    const result = await axios
+        .get(GET_TITLE_SEARCH_REVIEW_REQUEST_LIST_URL, config)
         .then(requestHandler<GetReviewTitleAndContentSearchRequestDto>)
         .catch(requestErrorHandler);
     return result;
-}
+};
 
 // function : 리뷰 게시물 작성자 검색 목록 불러오기 API 함수
-export const getTravelReviewWriterSearchRequest = async(searchWord: string) => {
-    const config = {params: {searchWord}}
-    const result = await axios.get(GET_WRITER_SEARCH_REVIEW_REQUEST_LIST_URL, config)
+export const getTravelReviewWriterSearchRequest = async (searchWord: string) => {
+    const config = { params: { searchWord } };
+    const result = await axios
+        .get(GET_WRITER_SEARCH_REVIEW_REQUEST_LIST_URL, config)
         .then(requestHandler<GetReviewWriterSearchRequestDto>)
         .catch(requestErrorHandler);
     return result;
-}
+};
 
 // function : 리뷰 게시물 작성일 검색 목록 불러오기 API 함수
-export const getTravelReviewWriteDateSearchRequest = async(searchWord: string) => {
-    const config = {params: {searchWord}}
-    const result = await axios.get(GET_WRITE_DATE_SEARCH_REVIEW_REQUEST_LIST_URL, config)
+export const getTravelReviewWriteDateSearchRequest = async (searchWord: string) => {
+    const config = { params: { searchWord } };
+    const result = await axios
+        .get(GET_WRITE_DATE_SEARCH_REVIEW_REQUEST_LIST_URL, config)
         .then(requestHandler<GetReviewWriteDateSearchRequestDto>)
         .catch(requestErrorHandler);
     return result;
-}
+};
 
 // function : 리뷰 게시물 상세보기 API 함수
-export const getTravelReviewDetailRequest = async(reviewNumber: number | string) => {
-    const result = await axios.get(GET_REVIEW_REQUEST_URL(reviewNumber))
+export const getTravelReviewDetailRequest = async (reviewNumber: number | string) => {
+    const result = await axios
+        .get(GET_REVIEW_REQUEST_URL(reviewNumber))
         .then(requestHandler<GetTravelReviewDetailResponseDto>)
         .catch(requestErrorHandler);
     return result;
-}
+};
 
 // function : 내가 쓴 리뷰 게시물 불러오기 API 함수
-export const getTravelReviewMyListRequest = async(accessToken: string) => {
-    const result = await axios.get(GET_REVIEW_MY_LIST_REQUEST_URL, bearerAuthorization(accessToken))
+export const getTravelReviewMyListRequest = async (accessToken: string) => {
+    const result = await axios
+        .get(GET_REVIEW_MY_LIST_REQUEST_URL, bearerAuthorization(accessToken))
         .then(requestHandler<GetTravelReviewMyListResponseDto>)
         .catch(requestErrorHandler);
     return result;
-}
+};
+
+// function : 내가 쓴 리뷰 게시물 제목 검색 목록 불러오기 API 함수
+export const getTravelReviewMyListSearchRequest = async (searchWord: string, accessToken: string) => {
+    const config = { ...bearerAuthorization(accessToken), params: { searchWord } };
+    const result = await axios
+        .get(GET_REVIEW_MY_LIST_REQUEST_URL, config)
+        .then(requestHandler<GetReviewTitleAndContentSearchRequestDto>)
+        .catch(requestErrorHandler);
+    return result;
+};
 
 // function : 해당 게시물에 유저가 좋아요 눌렀는지 상태 불러오기 API 함수
-export const getTravelReviewFavoriteStatusRequest = async(reviewNumber: number | string, accessToken: string) => {
-    const result = await axios.get(GET_REVIEW_FAVORITE_STATUS_REQUEST_URL(reviewNumber), bearerAuthorization(accessToken))
+export const getTravelReviewFavoriteStatusRequest = async (reviewNumber: number | string, accessToken: string) => {
+    const result = await axios
+        .get(GET_REVIEW_FAVORITE_STATUS_REQUEST_URL(reviewNumber), bearerAuthorization(accessToken))
         .then(requestHandler<GetTravelReviewFavoriteStatusResponseDto>)
         .catch(requestErrorHandler);
     return result;
-}
+};
 
 // function : 리뷰 게시물 조회수 증가 API 함수
-export const increaseViewCountRequest = async(reviewNumber: number | string) => {
-    const result = await axios.patch(PATCH_INCREASE_VIEW_COUNT_REQUEST_URL(reviewNumber))
+export const increaseViewCountRequest = async (reviewNumber: number | string) => {
+    const result = await axios
+        .patch(PATCH_INCREASE_VIEW_COUNT_REQUEST_URL(reviewNumber))
         .then(requestHandler<ResponseDto>)
         .catch(requestErrorHandler);
     return result;
-}
+};
 
 // function : 리뷰 게시물 좋아요 API 함수
-export const favoriteCountRequest = async(reviewNumber: number | string, accessToken: string) => {
-    const result = await axios.patch(PATCH_FAVORITE_COUNT_REQUEST_URL(reviewNumber),{}, bearerAuthorization(accessToken))
+export const favoriteCountRequest = async (reviewNumber: number | string, accessToken: string) => {
+    const result = await axios
+        .patch(PATCH_FAVORITE_COUNT_REQUEST_URL(reviewNumber), {}, bearerAuthorization(accessToken))
         .then(requestHandler<ResponseDto>)
         .catch(requestErrorHandler);
     return result;
-}
+};
 
 // function : 리뷰 게시물 삭제 API 함수
-export const deleteTravelReviewReqeust = async(reviewNumber : number | string, accessToken: string) => {
-    const result = await axios.delete(DELETE_REVIEW_REQUEST_URL(reviewNumber), bearerAuthorization(accessToken))
+export const deleteTravelReviewReqeust = async (reviewNumber: number | string, accessToken: string) => {
+    const result = await axios
+        .delete(DELETE_REVIEW_REQUEST_URL(reviewNumber), bearerAuthorization(accessToken))
         .then(requestHandler<ResponseDto>)
         .catch(requestErrorHandler);
     return result;
-}
+};
 
 // function : 리뷰 게시물의 댓글 & 답글 삭제 API 함수
-export const deleteTravelReviewCommentRequest = async(commentNumber: number | string, accessToken: string) => {
-    const result = await axios.delete(DELETE_REVIEW_COMMENT_REQUEST_URL(commentNumber), bearerAuthorization(accessToken))
+export const deleteTravelReviewCommentRequest = async (commentNumber: number | string, accessToken: string) => {
+    const result = await axios
+        .delete(DELETE_REVIEW_COMMENT_REQUEST_URL(commentNumber), bearerAuthorization(accessToken))
         .then(requestHandler<ResponseDto>)
         .catch(requestErrorHandler);
     return result;
-}
-
+};
--- src/constant/index.ts
+++ src/constant/index.ts
@@ -61,16 +61,12 @@ export const TOURATTRACTIONS_ABSOULUTE_PATH = `${SERVICE_PATH}/${TOUR_LIST_PATH}
 export const RESTAURANT_ABSOULUTE_PATH = `${SERVICE_PATH}/${TOUR_LIST_PATH}/${TOUR_PATH}`;
 
 export const QNA_WRITE_ABSOLUTE_PATH = `${SERVICE_PATH}/${QNA_PATH}/${QNA_WRITE_PATH}`;
-export const QNA_DETAIL_ABSOLUTE_PATH = (receptionNumber: string | number) =>
-    `${SERVICE_PATH}/${QNA_PATH}/${receptionNumber}`;
-export const QNA_UPDATE_ABSOLUTE_PATH = (receptionNumber: string | number) =>
-    `${SERVICE_PATH}/${QNA_PATH}/update/${receptionNumber}`;
+export const QNA_DETAIL_ABSOLUTE_PATH = (receptionNumber: string | number) => `${SERVICE_PATH}/${QNA_PATH}/${receptionNumber}`;
+export const QNA_UPDATE_ABSOLUTE_PATH = (receptionNumber: string | number) => `${SERVICE_PATH}/${QNA_PATH}/update/${receptionNumber}`;
 
 export const REVIEW_WRITE_ABSOLUTE_PATH = `${SERVICE_PATH}/${REVIEW_PATH}/${REVIEW_WRITE_PATH}`;
-export const REVIEW_DETAIL_ABSOLUTE_PATH = (reviewNumber: string | number) =>
-    `${SERVICE_PATH}/${REVIEW_PATH}/${reviewNumber}`;
-export const REVIEW_UPDATE_ABSOLUTE_PATH = (reviewNumber: string | number) =>
-    `${SERVICE_PATH}/${REVIEW_PATH}/update/${reviewNumber}`;
+export const REVIEW_DETAIL_ABSOLUTE_PATH = (reviewNumber: string | number) => `${SERVICE_PATH}/${REVIEW_PATH}/${reviewNumber}`;
+export const REVIEW_UPDATE_ABSOLUTE_PATH = (reviewNumber: string | number) => `${SERVICE_PATH}/${REVIEW_PATH}/update/${reviewNumber}`;
 
 export const MYPAGE_PROFILEUPDATE_ABSOLUTE_PAGE = `${SERVICE_PATH}/${MYPAGE_PATH}/${PROFILE_UPDATE_PATH}`;
 export const SCHEDULE_ABSOLUTE_PATH = `${SERVICE_PATH}/${MYPAGE_PATH}/${SCHEDULE_PATH}`;
@@ -78,12 +74,9 @@ export const MY_REVIEWLIST_ABSOLUTE_PATH = `${SERVICE_PATH}/${MYPAGE_PATH}/${MY_
 export const DELETE_USER_ABSOLUTE_PAGE = `${SERVICE_PATH}/${MYPAGE_PATH}/${DELETE_USER_PATH}`;
 
 export const SCHEDULE_WRITE_ABSOLUTE_PATH = `${SERVICE_PATH}/${MYPAGE_PATH}/${SCHEDULE_PATH}/${SCHEDULE_WRITE_PATH}`;
-export const SCHEDULE_DETAIL_ABSOLUTE_PATH = (
-    scheduleNumber: string | number
-) => `${SERVICE_PATH}/${MYPAGE_PATH}/${SCHEDULE_PATH}/${scheduleNumber}`;
-export const SCHEDULE_UPDATE_ABSOLUTE_PATH = (
-    scheduleNumber: string | number
-) => `${SERVICE_PATH}/${MYPAGE_PATH}/${SCHEDULE_PATH}/update/${scheduleNumber}`;
+export const SCHEDULE_DETAIL_ABSOLUTE_PATH = (scheduleNumber: string | number) => `${SERVICE_PATH}/${MYPAGE_PATH}/${SCHEDULE_PATH}/${scheduleNumber}`;
+export const SCHEDULE_UPDATE_ABSOLUTE_PATH = (scheduleNumber: string | number) =>
+    `${SERVICE_PATH}/${MYPAGE_PATH}/${SCHEDULE_PATH}/update/${scheduleNumber}`;
 
 export const ADMINPAGE_ABSOULUTE_PAGE = `${SERVICE_PATH}/${ADMINPAGE_PATH}`;
 export const ADMINPAGE_USER_LIST_ABSOLUTE_PATH = `${SERVICE_PATH}/${ADMINPAGE_PATH}/${USER_LIST_PATH}`;
@@ -92,24 +85,16 @@ export const ADMINPAGE_REST_LIST_ABSOLUTE_PATH = `${SERVICE_PATH}/${ADMINPAGE_PA
 export const ADMINPAGE_TOUR_ADD_ABSOLUTE_PATH = `${SERVICE_PATH}/${ADMINPAGE_PATH}/${TOUR_ADD_PATH}`;
 export const ADMINPAGE_REST_ADD_ABSOLUTE_PATH = `${SERVICE_PATH}/${ADMINPAGE_PATH}/${REST_ADD_PATH}`;
 
-export const ADMINPAGE_TOUR_CONTROL_ABSOLUTE_PATH = (
-    tourAttractionsNumber: string | number
-) => `${SERVICE_PATH}/${ADMINPAGE_PATH}/tourcontrol/${tourAttractionsNumber}`;
-export const ADMINPAGE_REST_CONTROL_ABSOLUTE_PATH = (
-    restaurantNumber: string | number
-) => `${SERVICE_PATH}/${ADMINPAGE_PATH}/restcontrol/${restaurantNumber}`;
+export const ADMINPAGE_TOUR_CONTROL_ABSOLUTE_PATH = (tourAttractionsNumber: string | number) =>
+    `${SERVICE_PATH}/${ADMINPAGE_PATH}/tourcontrol/${tourAttractionsNumber}`;
+export const ADMINPAGE_REST_CONTROL_ABSOLUTE_PATH = (restaurantNumber: string | number) =>
+    `${SERVICE_PATH}/${ADMINPAGE_PATH}/restcontrol/${restaurantNumber}`;
 
-export const TOURATTRACTIONS_DETAIL_ABSOLUTE_PATH = (
-    tourAttractionsNumber: string | number
-) => {
-    console.log(
-        `${SERVICE_PATH}/${TOUR_PATH}/tourlist/${tourAttractionsNumber}`
-    );
+export const TOURATTRACTIONS_DETAIL_ABSOLUTE_PATH = (tourAttractionsNumber: string | number) => {
+    console.log(`${SERVICE_PATH}/${TOUR_PATH}/tourlist/${tourAttractionsNumber}`);
     return `${SERVICE_PATH}/${TOUR_PATH}/tourlist/${tourAttractionsNumber}`;
 };
-export const RESTAURANT_DETAIL_ABSOLUTE_PATH = (
-    restaurantNumber: string | number
-) => `${SERVICE_PATH}/${TOUR_PATH}/restlist/${restaurantNumber}`;
+export const RESTAURANT_DETAIL_ABSOLUTE_PATH = (restaurantNumber: string | number) => `${SERVICE_PATH}/${TOUR_PATH}/restlist/${restaurantNumber}`;
 
 // description : API URL PATH
 export const SERVER_DOMAIN_URL = "http://localhost:4000";
@@ -117,8 +102,7 @@ export const SERVER_API_URL = `${SERVER_DOMAIN_URL}/traveldiary/v1`;
 
 export const SEVER_IMAGE_FILE_MODULE_URL = `${SERVER_API_URL}/image`;
 export const IMAGE_UPLOAD_URL = `${SEVER_IMAGE_FILE_MODULE_URL}/upload`;
-export const IMAGE_LOAD_URL = (imageUrl: string) =>
-    `${SEVER_IMAGE_FILE_MODULE_URL}/file/${imageUrl}`;
+export const IMAGE_LOAD_URL = (imageUrl: string) => `${SEVER_IMAGE_FILE_MODULE_URL}/file/${imageUrl}`;
 
 export const SERVER_AUTH_MODULE_URL = `${SERVER_API_URL}/auth`;
 export const SIGN_IN_REQUEST_URL = `${SERVER_AUTH_MODULE_URL}/sign-in`;
@@ -131,95 +115,69 @@ export const SIGN_OUT_REQUEST_URL = `${SERVER_AUTH_MODULE_URL}/sign-out`;
 
 export const SERVER_QNA_MODULE_URL = `${SERVER_API_URL}/qna`;
 export const POST_QNA_REQUEST_URL = `${SERVER_QNA_MODULE_URL}/`;
-export const POST_QNA_COMMENT_REQUEST_URL = (
-    receptionNumber: number | string
-) => `${SERVER_QNA_MODULE_URL}/${receptionNumber}/comment`;
+export const POST_QNA_COMMENT_REQUEST_URL = (receptionNumber: number | string) => `${SERVER_QNA_MODULE_URL}/${receptionNumber}/comment`;
 export const GET_QNA_LIST_REQUEST_URL = `${SERVER_QNA_MODULE_URL}/list`;
 export const GET_SEARCH_QNA_LIST_REQUEST_URL = `${SERVER_QNA_MODULE_URL}/list/search`;
-export const GET_QNA_REQUEST_URL = (receptionNumber: number | string) =>
-    `${SERVER_QNA_MODULE_URL}/list/${receptionNumber}`;
-export const PATCH_QNA_REQUEST_URL = (receptionNumber: number | string) =>
-    `${SERVER_QNA_MODULE_URL}/${receptionNumber}`;
-export const PATCH_QNA_COMMENT_REQUEST_URL = (
-    receptionNumber: number | string
-) => `${SERVER_QNA_MODULE_URL}/${receptionNumber}/comment`;
-export const DELETE_QNA_REQUEST_URL = (receptionNumber: number | string) =>
-    `${SERVER_QNA_MODULE_URL}/${receptionNumber}`;
-export const DELETE_QNA_COMMENT_REQUEST_URL = (
-    receptionNumber: number | string
-) => `${SERVER_QNA_MODULE_URL}/${receptionNumber}/comment`;
+export const GET_QNA_REQUEST_URL = (receptionNumber: number | string) => `${SERVER_QNA_MODULE_URL}/list/${receptionNumber}`;
+export const PATCH_QNA_REQUEST_URL = (receptionNumber: number | string) => `${SERVER_QNA_MODULE_URL}/${receptionNumber}`;
+export const PATCH_QNA_COMMENT_REQUEST_URL = (receptionNumber: number | string) => `${SERVER_QNA_MODULE_URL}/${receptionNumber}/comment`;
+export const DELETE_QNA_REQUEST_URL = (receptionNumber: number | string) => `${SERVER_QNA_MODULE_URL}/${receptionNumber}`;
+export const DELETE_QNA_COMMENT_REQUEST_URL = (receptionNumber: number | string) => `${SERVER_QNA_MODULE_URL}/${receptionNumber}/comment`;
 
 export const SERVER_REVIEW_MODULE_URL = `${SERVER_API_URL}/review`;
 export const SEARCH_REVIEW_MODULE_URL = `${SERVER_REVIEW_MODULE_URL}/list/search`;
+export const MY_SEARCH_REVIEW_MODULE_URL = `${SERVER_REVIEW_MODULE_URL}/post/list`;
 export const POST_REVIEW_REQUEST_URL = `${SERVER_REVIEW_MODULE_URL}/`;
-export const POST_REVIEW_COMMENT_REQUEST_URL = (
-    reviewNumber: number | string
-) => `${SERVER_REVIEW_MODULE_URL}/${reviewNumber}/comment`;
+export const POST_REVIEW_COMMENT_REQUEST_URL = (reviewNumber: number | string) => `${SERVER_REVIEW_MODULE_URL}/${reviewNumber}/comment`;
 export const GET_REVIEW_LIST_REQUEST_URL = `${SERVER_REVIEW_MODULE_URL}/list`;
 export const GET_REVIEW_MY_LIST_REQUEST_URL = `${SERVER_REVIEW_MODULE_URL}/post/list`;
+export const GET_REVIEW_MY_LIST_SEARCH_REQUEST_URL = `${MY_SEARCH_REVIEW_MODULE_URL}/search`;
 export const GET_TITLE_SEARCH_REVIEW_REQUEST_LIST_URL = `${SEARCH_REVIEW_MODULE_URL}/title`;
 export const GET_WRITER_SEARCH_REVIEW_REQUEST_LIST_URL = `${SEARCH_REVIEW_MODULE_URL}/writer`;
 export const GET_WRITE_DATE_SEARCH_REVIEW_REQUEST_LIST_URL = `${SEARCH_REVIEW_MODULE_URL}/writedate`;
-export const GET_COMMENT_LIST_REQUEST_URL = (reviewNumber: number | string) =>
-    `${SERVER_REVIEW_MODULE_URL}/${reviewNumber}/comment/list`;
-export const GET_REVIEW_REQUEST_URL = (reviewNumber: number | string) =>
-    `${SERVER_REVIEW_MODULE_URL}/${reviewNumber}`;
-export const GET_REVIEW_FAVORITE_STATUS_REQUEST_URL = (
-    reviewNumber: number | string
-) => `${SERVER_REVIEW_MODULE_URL}/${reviewNumber}/`;
-export const PATCH_REVIEW_REQUEST_URL = (reviewNumber: number | string) =>
-    `${SERVER_REVIEW_MODULE_URL}/${reviewNumber}`;
-export const PATCH_REVIEW_COMMENT_REQUEST_URL = (
-    commentNumber: number | string
-) => `${SERVER_REVIEW_MODULE_URL}/comment/${commentNumber}`;
-export const PATCH_INCREASE_VIEW_COUNT_REQUEST_URL = (
-    reviewNumber: number | string
-) => `${SERVER_REVIEW_MODULE_URL}/${reviewNumber}/view-count`;
-export const PATCH_FAVORITE_COUNT_REQUEST_URL = (
-    reviewNumber: number | string
-) => `${SERVER_REVIEW_MODULE_URL}/${reviewNumber}/favorite`;
-export const DELETE_REVIEW_REQUEST_URL = (reviewNumber: number | string) =>
-    `${SERVER_REVIEW_MODULE_URL}/delete/${reviewNumber}`;
-export const DELETE_REVIEW_COMMENT_REQUEST_URL = (
-    commentNumber: number | string
-) => `${SERVER_REVIEW_MODULE_URL}/comment/${commentNumber}`;
+export const GET_COMMENT_LIST_REQUEST_URL = (reviewNumber: number | string) => `${SERVER_REVIEW_MODULE_URL}/${reviewNumber}/comment/list`;
+export const GET_REVIEW_REQUEST_URL = (reviewNumber: number | string) => `${SERVER_REVIEW_MODULE_URL}/${reviewNumber}`;
+export const GET_REVIEW_FAVORITE_STATUS_REQUEST_URL = (reviewNumber: number | string) => `${SERVER_REVIEW_MODULE_URL}/${reviewNumber}/`;
+export const PATCH_REVIEW_REQUEST_URL = (reviewNumber: number | string) => `${SERVER_REVIEW_MODULE_URL}/${reviewNumber}`;
+export const PATCH_REVIEW_COMMENT_REQUEST_URL = (commentNumber: number | string) => `${SERVER_REVIEW_MODULE_URL}/comment/${commentNumber}`;
+export const PATCH_INCREASE_VIEW_COUNT_REQUEST_URL = (reviewNumber: number | string) => `${SERVER_REVIEW_MODULE_URL}/${reviewNumber}/view-count`;
+export const PATCH_FAVORITE_COUNT_REQUEST_URL = (reviewNumber: number | string) => `${SERVER_REVIEW_MODULE_URL}/${reviewNumber}/favorite`;
+export const DELETE_REVIEW_REQUEST_URL = (reviewNumber: number | string) => `${SERVER_REVIEW_MODULE_URL}/delete/${reviewNumber}`;
+export const DELETE_REVIEW_COMMENT_REQUEST_URL = (commentNumber: number | string) => `${SERVER_REVIEW_MODULE_URL}/comment/${commentNumber}`;
 
 export const SERVER_MYPAGE_MODULE_URL = `${SERVER_API_URL}/mypage`;
 export const SERVER_SCHEDULE_MODULE_URL = `${SERVER_API_URL}/schedule`;
 export const POST_SCHEDULE_REQUEST_URL = `${SERVER_SCHEDULE_MODULE_URL}/`;
 export const GET_SCHEDULE_LIST_REQUEST_URL = `${SERVER_SCHEDULE_MODULE_URL}/list`;
 export const GET_SEARCH_SCHEDULE_REQUEST_LIST_URL = `${SERVER_SCHEDULE_MODULE_URL}/list/search`;
-export const GET_SCHEDULE_REQUEST_URL = (scheduleNumber: number | string) =>
-    `${SERVER_SCHEDULE_MODULE_URL}/${scheduleNumber}`;
-export const PATCH_SCHEDULE_REQUEST_URL = (scheduleNumber: number | string) =>
-    `${SERVER_SCHEDULE_MODULE_URL}/${scheduleNumber}`;
-export const DELETE_SCHEDULE_REQUEST_URL = (scheduleNumber: number | string) =>
-    `${SERVER_SCHEDULE_MODULE_URL}/${scheduleNumber}`;
+export const GET_SCHEDULE_REQUEST_URL = (scheduleNumber: number | string) => `${SERVER_SCHEDULE_MODULE_URL}/${scheduleNumber}`;
+export const PATCH_SCHEDULE_REQUEST_URL = (scheduleNumber: number | string) => `${SERVER_SCHEDULE_MODULE_URL}/${scheduleNumber}`;
+export const DELETE_SCHEDULE_REQUEST_URL = (scheduleNumber: number | string) => `${SERVER_SCHEDULE_MODULE_URL}/${scheduleNumber}`;
 
 export const SERVER_USER_MODULE_URL = `${SERVER_API_URL}/user`;
 export const POST_USER_NICKNAME_REQUEST_URL = `${SERVER_USER_MODULE_URL}/nickName`;
 export const GET_USER_REQUEST_URL = `${SERVER_USER_MODULE_URL}/`;
 export const GET_USER_LIST_REQUEST_URL = `${SERVER_USER_MODULE_URL}/userlist`;
 export const GET_SEARCH_USER_LIST_REQUEST_URL = `${SERVER_USER_MODULE_URL}/userlist/search`;
-export const GET_USER_INFO_REQUEST_URL = (userId: number | string) =>
-    `${SERVER_USER_MODULE_URL}/${userId}`;
+export const GET_USER_INFO_REQUEST_URL = (userId: number | string) => `${SERVER_USER_MODULE_URL}/${userId}`;
 export const PATCH_USER_INFO_REQUEST_URL = `${SERVER_USER_MODULE_URL}/edit`;
 export const DELETE_USER_REQUEST_URL = `${SERVER_USER_MODULE_URL}/${DELETE_USER_PATH}`;
 export const DELETE_ADMIN_USER_REQUEST_URL = `${GET_USER_LIST_REQUEST_URL}/${DELETE_USER_PATH}`;
 
 export const SERVER_TOURATTRACTIONS_MODULE_URL = `${SERVER_API_URL}/tourattractions`;
 export const GET_TOURATTRACTIONS_LIST_REQUEST_URL = `${SERVER_TOURATTRACTIONS_MODULE_URL}/tourlist`;
-export const GET_TOURATTRACTIONS_REQUEST_URL = (
-    tourAttractionsNumber: number | string
-) => `${SERVER_TOURATTRACTIONS_MODULE_URL}/tourlist/${tourAttractionsNumber}`;
+export const GET_TOURATTRACTIONS_REQUEST_URL = (tourAttractionsNumber: number | string) =>
+    `${SERVER_TOURATTRACTIONS_MODULE_URL}/tourlist/${tourAttractionsNumber}`;
 export const GET_SEARCH_TOURATTRACTIONS_LIST_REQUEST_URL = `${SERVER_TOURATTRACTIONS_MODULE_URL}/tourlist/search`;
 export const POST_TOURATTRACTIONS_REQUEST_URL = `${SERVER_TOURATTRACTIONS_MODULE_URL}/addTourAttractions`;
-export const PATCH_TOURATTRACTIONS_REQUEST_URL = (tourAttractionsNumber: number | string) => `${SERVER_TOURATTRACTIONS_MODULE_URL}/tourlist/${tourAttractionsNumber}`;
-export const DELETE_TOURATTRACTIONS_REQUEST_URL = (tourAttractionsNumber: number | string) => `${SERVER_TOURATTRACTIONS_MODULE_URL}/tourlist/${tourAttractionsNumber}`;
+export const PATCH_TOURATTRACTIONS_REQUEST_URL = (tourAttractionsNumber: number | string) =>
+    `${SERVER_TOURATTRACTIONS_MODULE_URL}/tourlist/${tourAttractionsNumber}`;
+export const DELETE_TOURATTRACTIONS_REQUEST_URL = (tourAttractionsNumber: number | string) =>
+    `${SERVER_TOURATTRACTIONS_MODULE_URL}/tourlist/${tourAttractionsNumber}`;
 
 export const SERVER_RESTAURANT_MODULE_URL = `${SERVER_API_URL}/restaurant`;
 export const GET_RESTAURANT_LIST_REQUEST_URL = `${SERVER_RESTAURANT_MODULE_URL}/restlist`;
-export const GET_RESTAURANT_REQUEST_URL = (restaurantNumber: number | string) =>`${SERVER_RESTAURANT_MODULE_URL}/restlist/${restaurantNumber}`;
+export const GET_RESTAURANT_REQUEST_URL = (restaurantNumber: number | string) => `${SERVER_RESTAURANT_MODULE_URL}/restlist/${restaurantNumber}`;
 export const GET_SEARCH_RESTAURANT_LIST_REQUEST_URL = `${SERVER_RESTAURANT_MODULE_URL}/restlist/search`;
 export const POST_RESTAURANT_REQUEST_URL = `${SERVER_RESTAURANT_MODULE_URL}/addRestaurant`;
 export const PATCH_RESTAURANT_REQUEST_URL = (restaurantNumber: number | string) => `${SERVER_RESTAURANT_MODULE_URL}/restlist/${restaurantNumber}`;
