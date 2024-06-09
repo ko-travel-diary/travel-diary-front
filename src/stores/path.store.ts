d0d3518957f099b6e9051c2787bb2b9b770dce62
devjiraynor <jiraynor@naver.com>
2024-06-09 21:17:49 +0900

    main complete

---
+++ src/stores/path.store.ts
@@ -0,0 +1,14 @@
+import { Destination, Position } from "src/types";
+import { create } from "zustand";
+
+interface PathStore {
+    path: Position[];
+    setPath: (path: Position[]) => void;
+}
+
+const usePathStore = create<PathStore>(set => ({
+    path: [],
+    setPath: (path: Position[]) => set(state => ({ ...state, path })),
+}));
+
+export default usePathStore;
